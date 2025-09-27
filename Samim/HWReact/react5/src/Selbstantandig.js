dbcc0766cc08c88e89ab37d3ed1f85e81a6e50a9
Sulaimankahil <samimsulaimankhail02@gmail.com>
2025-09-27 18:21:21 +0200

    hw

---
+++ Samim/HWReact/react5/src/Selbstantandig.js
@@ -0,0 +1,15 @@
+const Selbstantandig=()=>{
+    return(
+        <div className="text-3xl bg-green-200  rounded-sm w-[50%] border-1 m-auto mt-5 p-5 bg-gray-300">
+<h1> 
+     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Eaque provident quia excepturi, maxime dolorem debitis. Saepe 
+     odit amet excepturi ad similique 
+    sapiente eum officia ill</h1>
+
+
+        </div>
+    )
+}
+
+export default Selbstantandig
