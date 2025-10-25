8115e4f37d9bf02d21190d908e23697bfbfb0bf9
Sulaimankahil <samimsulaimankhail02@gmail.com>
2025-10-25 18:28:44 +0200

    ubdet

--- Raihana/nextjsHW/homework1/src/app/not-found.jsx
+++ Raihana/nextjsHW/homework1/src/app/not-found.jsx
@@ -1,16 +1,22 @@
 "use client";
 
 export default function NotFound() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-center p-6 overflow-hidden animate-fadeIn">
-            {/* 404 Zahl */}
-            <h1 className="text-[8rem] font-extrabold text-[#E47A5C] drop-shadow-[0_10px_10px_#E47A5C] animate-slideUp delay-100">
-                404
-            </h1>
-            {/* Titel */}
-            <h2 className="text-2xl font-bold text-gray-800 mt-4 animate-slideUp delay-300">
-                Page not found 😢
-            </h2>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-center p-6 overflow-hidden animate-fadeIn">
+      {/* 404 Zahl */}
+      <h1 className="text-[8rem] font-extrabold text-[#E47A5C] drop-shadow-[0_10px_10px_#E47A5C] animate-slideUp delay-100">
+        404
+      </h1>
+      {/* Titel */}
+      <h2 className="text-2xl font-bold text-gray-800 mt-4 animate-slideUp delay-300">
+        Page not found 😢
+      </h2>
+      {/* Zurück-Button */}
+      <a
+        href="/html/link"
+        className="mt-6 px-6 py-3 bg-gradient-to-r from-[#E47A5C] to-[#3AA199] text-white font-semibold rounded-xl hover:opacity-90 transition animate-slideUp delay-500">
+        ← Back to Home
+      </a>
+    </div>
+  );
 }
