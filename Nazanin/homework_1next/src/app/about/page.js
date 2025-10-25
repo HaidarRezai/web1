e01ecbee6f84ad867f759e6e6d4f32e08eb56230
Ahmad Sulaimankhail <ahmadsulaimankhail034@gmail.com>
2025-10-26 07:09:51 +0930

    up

--- Nazanin/homework_1next/src/app/about/page.js
+++ Nazanin/homework_1next/src/app/about/page.js
@@ -1,7 +1,7 @@
 function About() {
     return (
         <div className="p-10">
-            <h1 className="text-4xl">about</h1>
+            <h1 className="text-4xl text-cyan-500">about</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime impedit perferendis voluptatem culpa, commodi obcaecati rem blanditiis deleniti dicta deserunt nam eligendi consequuntur aspernatur! Perferendis esse labore dicta praesentium eligendi.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime impedit perferendis voluptatem culpa, commodi obcaecati rem blanditiis deleniti dicta deserunt nam eligendi consequuntur aspernatur! Perferendis esse labore dicta praesentium eligendi.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime impedit perferendis voluptatem culpa, commodi obcaecati rem blanditiis deleniti dicta deserunt nam eligendi consequuntur aspernatur! Perferendis esse labore dicta praesentium eligendi.</p>
---
+++ Nazanin/homework_1next/src/app/home/home1/page.js
@@ -0,0 +1,12 @@
+
+function Home1() {
+    return (
+        <div className="mt-10 ml-10">
+            <h1 className="text-2xl text-cyan-500   ">LINK1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime impedit perferendis voluptatem culpa, commodi obcaecati rem blanditiis deleniti dicta deserunt nam eligendi consequuntur aspernatur! Perferendis esse labore dicta praesentium eligendi.</p>
+            <img src="/image/foto2.png" />
+
+        </div>
+    )
+}
+export default Home1;
